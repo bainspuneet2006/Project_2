7e6b0302f82e057e41484637d75b2fa9d3bf7495
jwaraich06 <156976836+jodhanwaraich@users.noreply.github.com>
2026-03-30 23:27:00 -0700

    Update stm32_transmitter.c

--- stm32_transmitter.c
+++ stm32_transmitter.c
@@ -1,200 +1,301 @@
-//  STM32 IR Transmitter - as simple as possible
-//  Protocol: send N pulses = command N, then 50ms gap, repeat forever
-
 #include "../Common/Include/stm32l051xx.h"
 #include "adc.h"
 #include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
 
-#define F_CPU          32000000L
-#define IR_FREQ        38000L
+// LQFP32 pinout
+//             ----------
+//       VDD -|1       32|- VSS
+//      PC14 -|2       31|- BOOT0
+//      PC15 -|3       30|- PB7
+//      NRST -|4       29|- PB6
+//      VDDA -|5       28|- PB5
+//       PA0 -|6       27|- PB4
+//       PA1 -|7       26|- PB3
+//  vx   PA2 -|8       25|- PA15
+//  vy   PA3 -|9       24|- PA14
+//switch PA4 -|10      23|- PA13
+// SW1   PA5 -|11      22|- PA12
+//SW 2   PA6 -|12      21|- PA11
+// SW 3  PA7 -|13      20|- PA10 (Reserved for RXD)
+//       PB0 -|14      19|- PA9  (Reserved for TXD)
+//       PB1 -|15      18|- PA8  (LED+1k)
+//       VSS -|16      17|- VDD
+//             ----------
+
+#define F_CPU 32000000L
+#define IR_FREQ 38000L
+
 #define HIGH_THRESHOLD 3000
-#define LOW_THRESHOLD  1000
+#define LOW_THRESHOLD 1000
 
-// Command numbers - just how many pulses to send
-#define CMD_STOP              1
-#define CMD_FORWARD           2
-#define CMD_BACKWARD          3
-#define CMD_LEFT              4
-#define CMD_RIGHT             5
-#define CMD_ROTATE            6
-#define CMD_SLIGHT_RIGHT      7
-#define CMD_SLIGHT_LEFT       8
-#define CMD_SLIGHT_BACK_RIGHT 9
-#define CMD_SLIGHT_BACK_LEFT  10
-#define CMD_PATH1             11
-#define CMD_PATH2             12
-#define CMD_PATH3             13
-#define CMD_CRUISE            14
+typedef enum {
+	STOP = 1,
+	FORWARD,
+	BACKWARD,
+	LEFT,
+	RIGHT,
+	ROTATE,
+	SLIGHT_RIGHT,
+	SLIGHT_LEFT,
+	SLIGHT_BACK_RIGHT,
+	SLIGHT_BACK_LEFT,
+	PATH1,
+	PATH2,
+	PATH3
 
-int joy_x, joy_y, press;
-int sw1, sw2, sw3, cruise_btn;
-int cmd;
+} Command;
 
-// ---------------------------------------------------------------------------
-// Timing
-// ---------------------------------------------------------------------------
+// Global Variables
+int joy_x;
+int joy_y;
+int press;
+int sw1;
+int sw2;
+int sw3;
+
+Command cmd;
+
 void wait_1ms(void)
 {
-    SysTick->LOAD = (F_CPU / 1000L) - 1;
-    SysTick->VAL  = 0;
-    SysTick->CTRL = SysTick_CTRL_CLKSOURCE_Msk | SysTick_CTRL_ENABLE_Msk;
-    while ((SysTick->CTRL & BIT16) == 0);
-    SysTick->CTRL = 0x00;
+	SysTick->LOAD = (F_CPU / 1000L) - 1;
+	SysTick->VAL = 0;
+	SysTick->CTRL = SysTick_CTRL_CLKSOURCE_Msk | SysTick_CTRL_ENABLE_Msk;
+	while ((SysTick->CTRL & BIT16) == 0);
+	SysTick->CTRL = 0x00;
 }
 
 void delayms(int len)
 {
-    while (len--) wait_1ms();
+	while (len--) wait_1ms();
 }
 
-// ---------------------------------------------------------------------------
-// UART for debug prints
-// ---------------------------------------------------------------------------
 void UART1_Init(void)
 {
-    RCC->IOPENR  |= BIT0;
-    RCC->APB2ENR |= BIT14;
-    GPIOA->MODER &= ~(BIT19 | BIT18);
-    GPIOA->MODER |=   BIT19;
-    GPIOA->AFR[1] &= ~(0xF << 4);
-    GPIOA->AFR[1] |=  (4   << 4);
-    USART1->BRR   = F_CPU / 115200;
-    USART1->CR1  |= BIT3 | BIT0;
+	RCC->IOPENR |= BIT0;
+	RCC->APB2ENR |= BIT14;
+
+	GPIOA->MODER &= ~(BIT19 | BIT18);
+	GPIOA->MODER |= BIT19;
+
+	GPIOA->AFR[1] &= ~(0xF << 4);
+	GPIOA->AFR[1] |= (4 << 4);
+
+	USART1->BRR = 32000000 / 115200;
+
+	USART1->CR1 |= BIT3;
+	USART1->CR1 |= BIT0;
 }
 
-int _write(int file, char *ptr, int len)
+int _write(int file, char* ptr, int len)
 {
-    int i;
-    for (i = 0; i < len; i++)
-    {
-        while (!(USART1->ISR & BIT7));
-        USART1->TDR = ptr[i];
-    }
-    return len;
+	int i;
+	for (i = 0; i < len; i++)
+	{
+		while (!(USART1->ISR & BIT7));
+		USART1->TDR = ptr[i];
+	}
+	return len;
 }
 
-// ---------------------------------------------------------------------------
-// GPIO - joystick, buttons
-// ---------------------------------------------------------------------------
 void GPIO_Init(void)
 {
-    RCC->IOPENR |= BIT0 | BIT1;
+	RCC->IOPENR |= BIT0; //GPIOA
+	RCC->IOPENR |= BIT1; //GPIOB
 
-    // PA2, PA3 = analog (joystick axes)
-    GPIOA->MODER |= (BIT4 | BIT5 | BIT6 | BIT7);
+	GPIOA->MODER = (GPIOA->MODER & ~(BIT4 | BIT5)) | (BIT4 | BIT5); // PA2 analog
+	GPIOA->MODER = (GPIOA->MODER & ~(BIT6 | BIT7)) | (BIT6 | BIT7); // PA3 analog
+	GPIOA->MODER = (GPIOA->MODER & ~(BIT8 | BIT9)); // PA4 input
+	GPIOA->PUPDR = (GPIOA->PUPDR & ~(BIT8 | BIT9)) | BIT8; // pull-up on PA4
 
-    // PA4=press, PA5=sw1, PA6=sw2, PA7=sw3 - inputs with pull-up
-    GPIOA->MODER &= ~(BIT8|BIT9|BIT10|BIT11|BIT12|BIT13|BIT14|BIT15);
-    GPIOA->PUPDR |=  (BIT8|BIT10|BIT12|BIT14);
+	GPIOA->MODER = (GPIOA->MODER & ~(BIT10 | BIT11)); // PA5 input
+	GPIOA->PUPDR = (GPIOA->PUPDR & ~(BIT10 | BIT11)) | BIT10; // pull-up on PA5
 
-    // PB0 = cruise button - input with pull-up
-    GPIOB->MODER &= ~(BIT0 | BIT1);
-    GPIOB->PUPDR |=   BIT0;
+	GPIOA->MODER = (GPIOA->MODER & ~(BIT12 | BIT13)); // PA6 input
+	GPIOA->PUPDR = (GPIOA->PUPDR & ~(BIT12 | BIT13)) | BIT12; // pull-up on PA6
+
+	GPIOA->MODER = (GPIOA->MODER & ~(BIT14 | BIT15)); // PA7 input
+	GPIOA->PUPDR = (GPIOA->PUPDR & ~(BIT14 | BIT15)) | BIT14; // pull-up on PA7
+
+	GPIOB->MODER = (GPIOB->MODER & ~(BIT0 | BIT1)); // PB0 input
+	GPIOB->PUPDR = (GPIOB->PUPDR & ~(BIT0 | BIT1)) | BIT0; // pull-up on PB0
 }
 
-// ---------------------------------------------------------------------------
-// IR 38kHz carrier on PA15 via TIM2_CH1
-// ---------------------------------------------------------------------------
 void IR_PWM_Init(void)
 {
-    RCC->IOPENR  |= BIT0;
-    RCC->APB1ENR |= BIT0;
+	RCC->IOPENR |= BIT0;
+	RCC->APB1ENR |= BIT0;
 
-    // PA15 alternate function 5 = TIM2_CH1
-    GPIOA->MODER  = (GPIOA->MODER  & ~(BIT30 | BIT31)) | BIT31;
-    GPIOA->AFR[1] = (GPIOA->AFR[1] & ~(0xF << 28))     | (5 << 28);
+	// PA15 = TIM2_CH1, AF5
+	GPIOA->OTYPER &= ~BIT15;
+	GPIOA->OSPEEDR = (GPIOA->OSPEEDR & ~(BIT30 | BIT31)) | BIT30;
+	GPIOA->MODER = (GPIOA->MODER & ~(BIT30 | BIT31)) | BIT31;
+	GPIOA->AFR[1] = (GPIOA->AFR[1] & ~(0xF << 28)) | (5 << 28);
 
-    TIM2->PSC   = 0;
-    TIM2->ARR   = (F_CPU / IR_FREQ) - 1;
-    TIM2->CCR1  = (TIM2->ARR + 1) / 2;   // 50% duty cycle
-    TIM2->CCMR1 = (6 << 4) | BIT3;       // PWM mode 1, preload enable
-    TIM2->CCER &= ~BIT0;                  // output off initially
-    TIM2->CR1  |= BIT7 | BIT0;
-    TIM2->EGR  |= BIT0;
+	TIM2->CR1 = 0;
+	TIM2->PSC = 0;
+	TIM2->ARR = (F_CPU / IR_FREQ) - 1;
+	TIM2->CCR1 = (TIM2->ARR + 1) / 2;
+
+	TIM2->CCMR1 &= ~(0xFF);
+	TIM2->CCMR1 |= (6 << 4);
+	TIM2->CCMR1 |= BIT3;
+
+	TIM2->CCER &= ~BIT0;
+	TIM2->CR1 |= BIT7;
+	TIM2->EGR |= BIT0;
+	TIM2->CR1 |= BIT0;
 }
 
-void carrier_on(void)  { TIM2->CCER |=  BIT0; }
-void carrier_off(void) { TIM2->CCER &= ~BIT0; }
-
-// ---------------------------------------------------------------------------
-// Read all inputs
-// ---------------------------------------------------------------------------
-void read_inputs(void)
+void carrier_on(void)
 {
-    joy_x      = readADC(ADC_CHSELR_CHSEL3);
-    joy_y      = readADC(ADC_CHSELR_CHSEL2);
-    press      = (GPIOA->IDR & BIT4) ? 0 : 1;
-    sw1        = (GPIOA->IDR & BIT5) ? 0 : 1;
-    sw2        = (GPIOA->IDR & BIT6) ? 0 : 1;
-    sw3        = (GPIOA->IDR & BIT7) ? 0 : 1;
-    cruise_btn = (GPIOB->IDR & BIT0) ? 0 : 1;
+	TIM2->CCER |= BIT0;
 }
 
-// ---------------------------------------------------------------------------
-// Pick command from inputs
-// ---------------------------------------------------------------------------
+void carrier_off(void)
+{
+	TIM2->CCER &= ~BIT0;
+}
+
+void read_joystick(void)
+{
+	joy_x = readADC(ADC_CHSELR_CHSEL3);
+	joy_y = readADC(ADC_CHSELR_CHSEL2);
+	press = (GPIOA->IDR & BIT4) ? 0 : 1;
+}
+
+void read_path_control(void)
+{
+	sw1 = (GPIOA->IDR & BIT5) ? 0 : 1;
+	sw2 = (GPIOA->IDR & BIT6) ? 0 : 1;
+	sw3 = (GPIOA->IDR & BIT7) ? 0 : 1;
+}
+
 void assign_command(void)
 {
-    if      (sw1)                                                 cmd = CMD_PATH1;
-    else if (sw2)                                                 cmd = CMD_PATH2;
-    else if (sw3)                                                 cmd = CMD_PATH3;
-    else if (cruise_btn)                                          cmd = CMD_CRUISE;
-    else if (press)                                               cmd = CMD_ROTATE;
-    else if (joy_y >= HIGH_THRESHOLD && joy_x >= HIGH_THRESHOLD) cmd = CMD_SLIGHT_RIGHT;
-    else if (joy_y >= HIGH_THRESHOLD && joy_x <= LOW_THRESHOLD)  cmd = CMD_SLIGHT_LEFT;
-    else if (joy_y <= LOW_THRESHOLD  && joy_x >= HIGH_THRESHOLD) cmd = CMD_SLIGHT_BACK_RIGHT;
-    else if (joy_y <= LOW_THRESHOLD  && joy_x <= LOW_THRESHOLD)  cmd = CMD_SLIGHT_BACK_LEFT;
-    else if (joy_y >= HIGH_THRESHOLD)                             cmd = CMD_FORWARD;
-    else if (joy_y <= LOW_THRESHOLD)                              cmd = CMD_BACKWARD;
-    else if (joy_x >= HIGH_THRESHOLD)                             cmd = CMD_RIGHT;
-    else if (joy_x <= LOW_THRESHOLD)                              cmd = CMD_LEFT;
-    else                                                          cmd = CMD_STOP;
+	cmd = STOP;
+
+	if (sw1) {
+		cmd = PATH1;
+	}
+	else if (sw2) {
+		cmd = PATH2;
+	}
+	else if (sw3) {
+		cmd = PATH3;
+	}
+	else {
+		if (press) {
+			cmd = ROTATE;
+		}
+		else if (joy_y >= HIGH_THRESHOLD && joy_x >= HIGH_THRESHOLD) {
+			cmd = SLIGHT_RIGHT;
+		}
+		else if (joy_y >= HIGH_THRESHOLD && joy_x <= LOW_THRESHOLD) {
+			cmd = SLIGHT_LEFT;
+		}
+		else if (joy_y <= LOW_THRESHOLD && joy_x >= HIGH_THRESHOLD) {
+			cmd = SLIGHT_BACK_RIGHT;
+		}
+		else if (joy_y <= LOW_THRESHOLD && joy_x <= LOW_THRESHOLD) {
+			cmd = SLIGHT_BACK_LEFT;
+		}
+		else if (joy_y >= HIGH_THRESHOLD) {
+			cmd = FORWARD;
+		}
+		else if (joy_y <= LOW_THRESHOLD) {
+			cmd = BACKWARD;
+		}
+		else if (joy_x >= HIGH_THRESHOLD) {
+			cmd = RIGHT;
+		}
+		else if (joy_x <= LOW_THRESHOLD) {
+			cmd = LEFT;
+		}
+		else {
+			cmd = STOP;
+		}
+	}
 }
 
-// ---------------------------------------------------------------------------
-// Send command: N pulses (5ms on, 5ms off), then 50ms end gap
-// Receiver just counts pulses before the long gap
-// ---------------------------------------------------------------------------
-void send_command(int n)
+void send_on(int ms)
 {
-    int i;
-    for (i = 0; i < n; i++)
-    {
-        carrier_on();
-        delayms(5);
-        carrier_off();
-        delayms(5);
-    }
-    delayms(50);  // long gap = end of message
+	carrier_on();
+	delayms(ms);
 }
 
-// ---------------------------------------------------------------------------
-// Main - read inputs, send command, repeat forever
-// ---------------------------------------------------------------------------
+void send_gap(int ms)
+{
+	carrier_off();
+	delayms(ms);
+}
+
+void send_bit(int bitval)
+{
+	if (bitval) {
+		send_on(4);
+		send_gap(3);
+	}
+	else {
+		send_on(3);
+		send_gap(4);
+	}
+}
+
+unsigned char cmd_to_hex(Command cmd)
+{
+	switch (cmd) {
+	case STOP:              return 0x01;
+	case FORWARD:           return 0x02;
+	case BACKWARD:          return 0x03;
+	case LEFT:              return 0x04;
+	case RIGHT:             return 0x05;
+	case ROTATE:            return 0x06;
+	case SLIGHT_RIGHT:      return 0x07;
+	case SLIGHT_LEFT:       return 0x08;
+	case SLIGHT_BACK_RIGHT: return 0x09;
+	case SLIGHT_BACK_LEFT:  return 0x0A;
+	case PATH1:             return 0x0B;
+	case PATH2:             return 0x0C;
+	case PATH3:             return 0x0D;
+	default:                return 0x01;
+	}
+}
+
+void send_byte(unsigned char hexval)
+{
+	int i;
+	for (i = 7; i >= 0; i--) {
+		send_bit((hexval >> i) & 0x01);
+	}
+}
+
+void send_command(Command cmd)
+{
+	unsigned char hexval = cmd_to_hex(cmd);
+	send_byte(hexval);
+	carrier_off();
+}
+
 void main(void)
 {
-    int last_cmd = -1;
+	GPIO_Init();
+	initADC();
+	UART1_Init();
+	setbuf(stdout, NULL);
+	IR_PWM_Init();
 
-    GPIO_Init();
-    initADC();
-    UART1_Init();
-    setbuf(stdout, NULL);
-    IR_PWM_Init();
+	while (1)
+	{
+		read_path_control();
+		read_joystick();
+		assign_command();
 
-    printf("\r\nSTM32 IR TX READY\r\n");
+		printf("X=%d Y=%d BTN=%d CMD=%d\r\n", joy_x, joy_y, press, cmd);
 
-    while (1)
-    {
-        read_inputs();
-        assign_command();
+		send_command(cmd);
 
-        // Only print when command changes
-        if (cmd != last_cmd)
-        {
-            printf("TX: %d\r\n", cmd);
-            last_cmd = cmd;
-        }
-
-        send_command(cmd);  // sends then waits 50ms, then loops back
-    }
+		delayms(50);
+	}
 }
